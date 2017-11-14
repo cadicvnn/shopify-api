6dcff1a46e743a9dfa22a86f2bbae4c4332b760c
Kien <kienptit180995@gmail.com>
2017-11-14 16:55:30 +0700

    add Collect, Product Apis

---
+++ src/Secomapp/Resources/Collect.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Secomapp\Resources;
+
+use Secomapp\BaseResource;
+
+class Collect extends BaseResource
+{
+	public function all($params = []){
+        return $this->client->get('collects.json', 'collects', $params);
+    }
+
+    public function get($customCollectionId, $params, $fields = null){
+        $params = $this->prepareFields($fields);
+        return $this->client->get("collects.json?{$customCollectionId}", 'collects', $params);
+    }
+
+    public function create($params){
+        $params = ['collect' => $params];
+        return $this->client->post('collects.json', 'collect', $params);
+    }
+    public function delete($id){
+        return $this->client->delete("collects/{$id}.json");
+    }
+}
---
+++ src/Secomapp/Resources/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Secomapp\Resources;
+
+use Secomapp\BaseResource;
+
+class Product extends BaseResource
+{
+    public function all($params = []){
+        return $this->client->get('products.json', 'products', $params);
+    }
+    public function update($id, $product){
+    	$params = ['product' => $product];
+    	return $this->client->put("products/{$id}.json", 'product', $params);
+    }
+    public function delete($id){
+        return $this->client->delete("products/{$id}.json");
+    }
+}
