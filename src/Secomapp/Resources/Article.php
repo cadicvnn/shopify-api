f200585be05828f93d19f291094a3740fc7317fd
quanghung97 <hungcan1997@gmail.com>
2019-02-14 17:36:38 +0700

    add Article, Blog api

---
+++ src/Secomapp/Resources/Article.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Secomapp\Resources;
+
+use Secomapp\BaseResource;
+use Secomapp\Exceptions\ShopifyApiException;
+use stdClass;
+
+class Article extends BaseResource
+{
+    /**
+     * Retrieves a list of all articles from a blog.
+     *
+     * @param string $blogId The Id of the blog will be associated with.
+     *
+     * @throws ShopifyApiException
+     *
+     * @return array
+     */
+    public function all($blogId)
+    {
+        return $this->client->get("blogs/{$blogId}/articles.json", 'articles');
+    }
+
+    /**
+     * Retrieves a count of all articles from a blog.
+     *
+     * @param string $blogId The Id of the blog will be associated with.
+     *
+     * @throws ShopifyApiException
+     *
+     * @return int
+     */
+    public function count($blogId)
+    {
+        return $this->client->get("blogs/{$blogId}/articles/count.json", 'count');
+    }
+
+    /**
+     * Receive a single Article.
+     *
+     * @param string $blogId
+     * @param string $articleId
+     *
+     * @throws ShopifyApiException
+     *
+     * @return stdClass
+     */
+    public function get($blogId, $articleId)
+    {
+        return $this->client->get("blogs/{$blogId}/articles/{$articleId}.json", 'article');
+    }
+
+    /**
+     * Creates an article for a blog.
+     *
+     * @param string $blogId
+     * @param array $params
+     *
+     * @throws ShopifyApiException
+     *
+     * @return stdClass
+     */
+    public function create($blogId, $params)
+    {
+        return $this->client->post("blogs/{$blogId}/articles.json", 'article', [
+            'article' => $params,
+        ]);
+    }
+
+    /**
+     * Updates an article.
+     *
+     * @param string $blogId
+     * @param string $articleId
+     * @param array  $params
+     *
+     * @throws ShopifyApiException
+     *
+     * @return stdClass
+     */
+    public function update($blogId, $articleId, $params)
+    {
+        return $this->client->put("blogs/{$blogId}/articles/{$articleId}.json", 'article', [
+            'article' => $params,
+        ]);
+    }
+
+    /**
+     * Retrieves a list all of article authors.
+     *
+     * @param array $params
+     *
+     * @throws ShopifyApiException
+     *
+     * @return array
+     */
+    public function authors($params = [])
+    {
+        return $this->client->get('articles/authors.json', 'authors', $params);
+    }
+
+    /**
+     * Retrieves a list of all the tags.
+     *
+     * @param array $params
+     *
+     * @throws ShopifyApiException
+     *
+     * @return array
+     */
+    public function tags($params = [])
+    {
+        return $this->client->get('articles/tags.json', 'tags', $params);
+    }
+
+    /**
+     * Delete an article.
+     *
+     * @param string $blogId
+     * @param string $articleId
+     *
+     * @throws ShopifyApiException
+     */
+    public function delete($blogId, $articleId)
+    {
+        $this->client->delete("blogs/{$blogId}/articles/{$articleId}.json");
+    }
+}
---
+++ src/Secomapp/Resources/Blog.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Secomapp\Resources;
+
+use Secomapp\BaseResource;
+use Secomapp\Exceptions\ShopifyApiException;
+use stdClass;
+
+class Blog extends BaseResource
+{
+    /**
+     * Receive a list of all Blogs.
+     *
+     * @param array $params
+     *
+     * @throws ShopifyApiException
+     *
+     * @return array
+     */
+    public function all($params = [])
+    {
+        return $this->client->get('blogs.json', 'blogs', $params);
+    }
+
+    /**
+     * Receive a count of all Blogs.
+     *
+     * @throws ShopifyApiException
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return $this->client->get('blogs/count.json', 'count');
+    }
+
+    /**
+     * Get a single blog by its ID.
+     *
+     * @param string $blogId
+     *
+     * @throws ShopifyApiException
+     *
+     * @return stdClass
+     */
+    public function get($blogId)
+    {
+        return $this->client->get("blogs/{$blogId}.json", 'blog');
+    }
+
+    /**
+     * Create a new blog.
+     *
+     * @param array $params
+     *
+     * @throws ShopifyApiException
+     *
+     * @return stdClass
+     */
+    public function create($params)
+    {
+        return $this->client->post("blogs.json", 'blog', [
+            'blog' => $params,
+        ]);
+    }
+
+    /**
+     * Update a blog.
+     *
+     * @param string $blogId
+     * @param array  $params
+     *
+     * @throws ShopifyApiException
+     *
+     * @return stdClass
+     */
+    public function update($blogId, $params)
+    {
+        return $this->client->put("blogs/{$blogId}.json", 'blog', [
+            'blog' => $params,
+        ]);
+    }
+
+    /**
+     * Delete a blog.
+     *
+     * @param string $blogId
+     *
+     * @throws ShopifyApiException
+     */
+    public function delete($blogId)
+    {
+        $this->client->delete("blogs/{$blogId}.json");
+    }
+}
